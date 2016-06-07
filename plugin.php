fa4ac3fb716422fe80d1352d8a18e4b049af491a
ben_ <ben_@benjamins-mbp.speedport_w_921v_1_39_000>
2016-06-07 11:45:06 +0200

    First Commit

---
+++ plugin.php
@@ -0,0 +1,10 @@
+<?php 
+/**
+ * Plugin Name: PALASTHOTEL 
+ * Description: 
+ * Version: 1.0
+ * Author: PALASTHOTEL by 
+ * Author URI: http://palasthotel.de/
+ * License: GPLv2 or later
+ * 
+ */	
